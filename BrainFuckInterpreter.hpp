5d92a7dcf41e26f51e33675ffedeb264d9c1b538
dotFoxyyyy <muxxum65536@gmail.com>
2021-11-26 17:22:37 +0900

    oop-ified.

---
+++ BrainFuckInterpreter.hpp
@@ -0,0 +1,236 @@
+#include <iostream>
+#include <set>
+#include <stack>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <exception>
+#include <map>
+#include <memory>
+
+using namespace std;
+
+struct RuntimeStack {
+private:
+    void release_handle() {
+        stk = nullptr;
+        p = nullptr;
+    }
+
+public:
+    char *stk = nullptr;
+    char *p = nullptr;
+    size_t stk_size = 0;
+
+    explicit RuntimeStack(size_t size) {
+        stk = new char[size];
+        for (size_t i = 0; i < size; ++i) stk[i] = 0;
+        p = stk;
+        stk_size = size;
+    }
+
+    RuntimeStack(const RuntimeStack &) = delete;
+
+    RuntimeStack &operator=(const RuntimeStack &) = delete;
+
+    RuntimeStack(RuntimeStack &&r_stk) noexcept {
+        this->stk = r_stk.stk;
+        this->p = r_stk.p;
+        r_stk.release_handle();
+    }
+
+    RuntimeStack &operator=(RuntimeStack &&r_stk) noexcept {
+        this->stk = r_stk.stk;
+        this->p = r_stk.p;
+        r_stk.release_handle();
+        return *this;
+    }
+
+    ~RuntimeStack() {
+        delete[] stk;
+    }
+
+public:
+    void show() const {
+        for (size_t i = 0; i < stk_size; ++i) {
+            if (i % 10 == 0 && i > 9)
+                cout << ",\n";
+            cout << '[' << (int) stk[i] << "] " << flush;
+        }
+    }
+};
+
+struct JumpTable {
+    typedef map<size_t, size_t> jump_map;
+    jump_map left_right, right_left;
+
+    explicit JumpTable(const string &instructions) {
+        size_t position = 0;
+        stack<size_t> stk;
+        auto stk_pop = [&stk]() -> size_t {
+            auto ret = stk.top();
+            stk.pop();
+            return ret;
+        };
+        for (; position < instructions.size(); ++position) {
+            if (instructions[position] == '[')
+                stk.push(position);
+            if (instructions[position] == ']')
+                right_left[position] = stk_pop();
+        }
+
+        for (const auto&[k, v]: right_left)
+            left_right[v] = k;
+    }
+};
+
+class Preprocessor {
+private:
+    string path;
+private:
+    string scan_code() {
+        std::ifstream f_i(path, ios::in);
+        if (!f_i.is_open()) {
+            throw runtime_error("Can not open file: " + path);
+        }
+        stringstream ss;
+        ss << f_i.rdbuf();
+        return ss.str();
+    };
+
+    static string pre_process(const string &source) {
+        stringstream ss;
+        set<char> skips = {'\0', ' ', '\n', '\t'};
+        for (const auto &ch: source)
+            if (skips.find(ch) == skips.end())
+                ss << ch;
+        return ss.str();
+    };
+
+    static string lexical_check(const string &code) {
+        set<char> valid_instructions = {
+                '<', '>', ',', '.', '+', '-', '[', ']',
+                ';', ':'
+        };
+        stack<char> stk;
+        for (const auto &ch: code) {
+            if (valid_instructions.find(ch) == valid_instructions.end()) {
+                string msg = "invalid instruction:";
+                msg += ch;
+                throw invalid_argument(msg);
+            }
+
+            if (ch == '[')
+                stk.push('[');
+            if (ch == ']') {
+                auto top = stk.top();
+                stk.pop();
+                if (top != '[')
+                    throw invalid_argument("SyntaxError: Unclosed Loop");
+            }
+        }
+        if (!stk.empty())throw invalid_argument("SyntaxError: Unclosed Loop");
+
+        return code;
+    }
+
+public:
+    explicit Preprocessor(const string &path) {
+        this->path = path;
+    }
+
+    string process() {
+        return lexical_check(pre_process(scan_code()));
+    }
+};
+
+class Executor {
+private:
+    unique_ptr<JumpTable> table = nullptr;
+    string instructions;
+    unique_ptr<RuntimeStack> stk = nullptr;
+private:
+    static void write_console(const char *p) {
+        cout << *p << flush;
+    }
+
+    void show_runtime_stk() {
+        stk->show();
+    }
+
+    [[maybe_unused]] void show_instructions() const noexcept {
+        cout << "Try to interpret: \n{\n\t" << instructions << "\n}" << endl;
+    }
+
+    static char read_char() {
+        char ret;
+        cin >> ret;
+        return ret;
+    }
+
+public:
+    explicit Executor(const string &instructions, size_t stk_size) {
+        this->instructions = instructions;
+        table = make_unique<JumpTable>(instructions);
+        stk = make_unique<RuntimeStack>(stk_size);
+    }
+
+    int execute() {
+        size_t position = 0;
+        char *runtime_stk = stk->stk, *p = stk->p;
+        while (position < instructions.size()) {
+            char ins = instructions[position];
+            switch (ins) {
+                case '>':
+                    if (p == runtime_stk + this->stk->stk_size)
+                        throw overflow_error(
+                                "RuntimeError:can not move pc pointer anymore, pc pointer is out of stack.");
+                    p++;
+                    break;
+                case '<':
+                    if (p == runtime_stk)
+                        throw underflow_error("RuntimeError:pc pointer is now points to the top of rt stack.");
+                    p--;
+                    break;
+                case '+':
+                    (*p)++;
+                    break;
+                case '-':
+                    (*p)--;
+                    break;
+                case '.':
+                    write_console(p);
+                    break;
+                case ',':
+                    *p = read_char();
+                    break;
+                case '[':
+                    position = *p == 0 ? table->left_right[position] : position;
+                    break;
+                case ']':
+                    position = *p != 0 ? table->right_left[position] : position;
+                    break;
+                case ';':
+                    show_runtime_stk();
+                    break;
+                case ':':
+                    cout << p - runtime_stk << flush;
+                    break;
+                default:
+                    cerr << "Interpreter Error" << endl;
+                    return -1;
+            }
+            position++;
+        }
+        return 0;
+    }
+};
+
+struct Interpreter {
+    int operator()(const string &path) {
+        Preprocessor preprocessor(R"(G:\BrainFuck\tester\rw.bf)");
+        Executor executor(preprocessor.process(), 300);
+        return executor.execute();
+
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
-#include "bf_interpreter_functions.hpp"
+#include "BrainFuckInterpreter.hpp"
+
+// Trying to oop-ify this Interpreter
 int main(int argc, char *argv[]) {
-    const string instructions = pre_process(scan_source(R"(G:\BrainFuck\tester\rw.bf)")); // should be argv[1]
-    instructions_check(instructions);
-    jump_map right_to_left, left_to_right;
-    build_jump_table(right_to_left,left_to_right, instructions);
-    RuntimeStack stk(STACK_SIZE);
-    execute(instructions, right_to_left, left_to_right, stk);
-    return 0;
+    Interpreter interpreter;
+    return interpreter(argv[1]);
 }
